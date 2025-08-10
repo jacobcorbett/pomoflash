5aac1a5835241badb325bcb6cf74c54c8c0be296
jacobcorbett <jacobdcorbett86@gmail.com>
2025-08-10 10:09:41 +0100

    Initial Commit

---
+++ PomoFlash/PomoFlashApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoFlashApp.swift
+//  PomoFlash
+//
+//  Created by Jacob Corbett on 10/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PomoFlashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PomoFlashTests/PomoFlashTests.swift
@@ -0,0 +1,17 @@
+//
+//  PomoFlashTests.swift
+//  PomoFlashTests
+//
+//  Created by Jacob Corbett on 10/08/2025.
+//
+
+import Testing
+@testable import PomoFlash
+
+struct PomoFlashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
